632892c0855c0f2da0994116b5c942b8d72b81b3
jimbo1qaz <jimbo1qaz@gmail.com>
2018-10-02 20:39:57 -0700

    Cleanup, .gitignore

--- build.cmd
+++ build.cmd
@@ -3,9 +3,6 @@ call "C:\Users\jimbo1qaz\encrypted\code\amk_tools\venv\Scripts\activate.bat"
 set folder=..\addmusick-1.1.0-beta\
 
 set NAME=zmm-final
-rem set NAME=foo
-rem set NAME=fuck
-
 
 python -m amktools.wav2brr --verbose ^
 	"wav" %folder% "idol1 final hours" || goto :eof
